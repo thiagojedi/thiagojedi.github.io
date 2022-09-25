3f7e4ab468c95c73fb32196fdb0790f004b6e291
Thiago 'Jedi' Cerqueira <thiago.cerqueira@agilecontent.com>
2022-09-24 21:59:34 -0300

    Fix spacing on header

--- src/_components/header.tsx
+++ src/_components/header.tsx
@@ -3,7 +3,7 @@ const Header = () => {
     <header>
       <div>
         <h1>
-          <a href="/">JEDIComp</a>
+          <a href="/">JEDIComp</a>{' '}
           <small>desenvolvendo à Força</small>
         </h1>
       </div>
