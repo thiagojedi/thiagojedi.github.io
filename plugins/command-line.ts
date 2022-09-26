4ae6a50eea471a783f8575e34c5820535a367cc6
Thiago 'Jedi' Cerqueira <thiago.cerqueira@agilecontent.com>
2022-09-26 12:31:20 -0300

    Add prompt text option

--- plugins/command-line.ts
+++ plugins/command-line.ts
@@ -3,12 +3,36 @@ import type { Site } from "lume/core.ts";
 const getAttribute = (e: unknown, attr: string) => {
   const element = <Element> e;
   const value = element.getAttribute(attr);
-  element.removeAttribute(attr);
+  if (value !== null) {
+    element.removeAttribute(attr);
+  }
   return value;
 };
 
+type CommandLineOptions = {
+  /**
+   * User in the prompt. Defaults to "user"
+   * @example user@server $
+   */
+  user: string;
+  /**
+   * Server in the prompt. Defaults to "localhost"
+   * @example user@localhost $
+   */
+  server: string;
+  /**
+   * Default prompt. Takes precedence over user and server options.
+   * @example PS C:\>
+   */
+  prompt: string;
+};
+
 export default function commandLine(
-  { user = "user", server = "localhost" } = {},
+  {
+    user: defaultUser = "user",
+    server: defaultServer = "localhost",
+    prompt: defaultPrompt,
+  }: Partial<CommandLineOptions> = {},
 ) {
   return (site: Site) => {
     site.process([".html"], ({ document }) => {
@@ -20,24 +44,40 @@ export default function commandLine(
       elements.forEach((element) => {
         const dataUser = getAttribute(element, "user");
         const dataServer = getAttribute(element, "server");
+        const dataPrompt = getAttribute(element, "prompt");
 
-        const promptText = `${dataUser ?? user}@${dataServer ?? server} $ `;
+        let promptText;
+        if (dataPrompt) {
+          promptText = dataPrompt;
+        } else if (dataUser || dataServer) {
+          const promptUser = dataUser ?? defaultUser;
+          const promptMachine = dataServer ?? defaultServer;
+          const promptSign = promptUser === "root" ? "#" : "$";
+
+          promptText = `${promptUser}@${promptMachine} ${promptSign}`;
+        } else {
+          const promptSign = defaultUser === "root" ? "#" : "$";
+          promptText = defaultPrompt ??
+            `${defaultUser}@${defaultServer} ${promptSign}`;
+        }
 
         const codeLines = element.firstChild.textContent.trim().split("\n");
+        const numberOfLines = codeLines.length;
 
-        element.removeChild(element.firstChild);
+        const promptColumn = document.createElement("span");
+        promptColumn.className = "command-line-prompt";
 
-        codeLines.forEach((line) => {
-          const br = document.createElement("br");
+        for (let index = 0; index < numberOfLines; index++) {
           const prompt = document.createElement("span");
-          prompt.classList.add("token", "prompt");
-          prompt.textContent = promptText;
+          prompt.textContent = promptText + " ";
 
-          const codeLine = document.createTextNode(line);
-          element.appendChild(prompt);
-          element.appendChild(codeLine);
-          element.appendChild(br);
-        });
+          const br = document.createElement("br");
+
+          promptColumn.appendChild(prompt);
+          promptColumn.appendChild(br);
+        }
+
+        element.insertBefore(promptColumn, element.firstChild);
       });
     });
   };
