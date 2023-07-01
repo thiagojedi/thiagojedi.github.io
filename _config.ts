a42370d1523aff3665221f9736dc37ba6a5d874e
Thiago Jedi <thiagoa7@gmail.com>
2023-07-01 18:57:19 -0300

    Update deps

--- _config.ts
+++ _config.ts
@@ -1,8 +1,16 @@
 import lume from "lume/mod.ts";
-import jsx_preact from "lume/plugins/jsx_preact.ts";
+import jsx from "lume/plugins/jsx_preact.ts";
 import metas from "lume/plugins/metas.ts";
 import prism from "lume/plugins/prism.ts";
 import commandLine from "./plugins/command-line.ts";
+
+import "npm:prismjs@1.29.0/components/prism-javascript.js";
+import "npm:prismjs@1.29.0/components/prism-typescript.js";
+import "npm:prismjs@1.29.0/components/prism-jsx.js";
+import "npm:prismjs@1.29.0/components/prism-tsx.js";
+import "npm:prismjs@1.29.0/components/prism-json.js";
+import "npm:prismjs@1.29.0/components/prism-markup.js";
+import "npm:prismjs@1.29.0/components/prism-sql.js";
 
 const site = lume({
   location: new URL("https://thiagojedi.github.io"),
@@ -11,11 +19,9 @@ const site = lume({
   .loadAssets([".css"])
   .copy("assets", ".");
 
-site.use(jsx_preact());
+site.use(jsx());
 site.use(metas());
-site.use(prism({
-  languages: ["js", "ts", "jsx", "tsx", "json", "html", "sql"],
-}));
+site.use(prism());
 site.use(commandLine());
 
 export default site;
