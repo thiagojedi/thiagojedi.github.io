38c440d5b181cc7918dd858ba2b718d223c0a14e
Thiago 'Jedi' Cerqueira <thiagoa7@gmail.com>
2024-08-24 14:14:16 -0300

    Update lume version

--- _config.ts
+++ _config.ts
@@ -1,5 +1,5 @@
 import lume from "lume/mod.ts";
-import jsx from "lume/plugins/jsx_preact.ts";
+import jsx_preact from "lume/plugins/jsx_preact.ts";
 import metas from "lume/plugins/metas.ts";
 import prism from "lume/plugins/prism.ts";
 import commandLine from "./plugins/command-line.ts";
@@ -13,14 +13,14 @@ import "npm:prismjs@1.29.0/components/prism-markup.js";
 import "npm:prismjs@1.29.0/components/prism-sql.js";
 
 const site = lume({
-  location: new URL("https://thiagojedi.github.io"),
-  src: "./src/",
-})
-  .ignore("helpers", "services", "types")
-  .loadAssets([".css"])
-  .copy("assets", ".");
+    location: new URL("https://thiagojedi.github.io"),
+    src: "./src/",
+});
 
-site.use(jsx());
+site.loadAssets([".css"]);
+site.copy("assets", ".");
+
+site.use(jsx_preact());
 site.use(metas());
 site.use(prism());
 site.use(commandLine());
