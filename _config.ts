f2689fe682cd4562314fe58ac6ff1437cd16edce
Thiago 'Jedi' Cerqueira <thiagoa7@gmail.com>
2025-05-07 20:15:13 -0300

    Upgrade to lume 3

--- _config.ts
+++ _config.ts
@@ -1,5 +1,5 @@
 import lume from "lume/mod.ts";
-import jsx_preact from "lume/plugins/jsx_preact.ts";
+import jsx from "lume/plugins/jsx.ts";
 import metas from "lume/plugins/metas.ts";
 import prism from "lume/plugins/prism.ts";
 import commandLine from "./plugins/command-line.ts";
@@ -13,14 +13,14 @@ import "npm:prismjs@1.29.0/components/prism-markup.js";
 import "npm:prismjs@1.29.0/components/prism-sql.js";
 
 const site = lume({
-    location: new URL("https://thiagojedi.github.io"),
-    src: "./src/",
+  // location: new URL("https://thiagojedi.github.io"),
+  src: "./src/",
 });
 
-site.loadAssets([".css"]);
-site.copy("assets", ".");
+site.add([".css"]);
+site.add("assets", ".");
 
-site.use(jsx_preact());
+site.use(jsx());
 site.use(metas());
 site.use(prism());
 site.use(commandLine());
