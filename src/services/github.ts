3dfcf850892dbcd569c3db3aed0f1e6c4c1c9aea
Thiago 'Jedi' Cerqueira <thiagoa7@gmail.com>
2025-05-07 21:45:13 -0300

    Use codeberg rather than github io

--- src/services/github.ts
+++ src/services/github.ts
@@ -1,18 +1,11 @@
 import { Octokit } from "octokit";
 
-const octokit = new Octokit();
-
-export type GithubRepo = {
-  language?: string | null;
-  html_url: string;
-  name: string;
-  description?: string | null;
-  fork: boolean;
-  updated_at?: string | null;
-};
-
-export async function getPublicRepositories(): Promise<GithubRepo[]> {
+export async function getPublicRepositories() {
   try {
+    const octokit = new Octokit({
+      baseUrl: "https://codeberg.org/api/v1",
+    });
+
     const repos = await octokit.rest.repos.listForUser({
       username: "thiagojedi",
       per_page: 6,
@@ -22,7 +15,7 @@ export async function getPublicRepositories(): Promise<GithubRepo[]> {
 
     return repos.data;
   } catch (e) {
-    console.error(e)
+    console.error(e);
     console.error("error trying to get repos from github");
   }
 
